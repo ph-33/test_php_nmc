2b68b3de5d74cfe2bcf3caff3f4884058a31bebc
minh cuong <cuongnguyen.210998@gmail.com>
2019-05-29 18:22:51 +0700

    bcb

--- test.php
+++ test.php
@@ -1,11 +1,45 @@
-<?php
-$a =4;
-$b =5;
 
-$c = $a ;
-$a = $b ;
-$b = $c;
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hoan doi vi tri</title>
+	<style >
+		.*{
+		margin:0;
+		padding:0;
+	}
+	</style>
+</head>
+<body>
+	<?php
 
-
-echo $a,$b;
+	$numbera = $_POST['so_a'];
+	$numberb =$_POST['so_b'];
+	$numberC = '';
+	function swap(&$number1, &$number2)
+    	{
+        	$numberC   = $number1;
+        	$number1  = $number2;
+        	$number2 = $numberC;
+    	}
+    	swap($numbera, $numberb);
 ?>
+		<form action="" method="post">
+			<label for="">A</label>	
+			<input type="text" name="so_a">
+			
+			<label for="">B</label>	
+			<input type="text" name="so_b">
+			
+			 <div id="buttons">
+                <label>&nbsp;</label>
+                <input type="submit" value="Hoan doi"/><br/>
+            </div>
+		</form>
+		<?php     
+		echo $numbera;
+    	echo $numberb; 
+    	?>
+</body>
+</html>
+
