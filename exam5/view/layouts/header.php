1a57ab4b66a8eddf84e15cdaeb2bd827934f91a0
minh cuong <cuongnguyen.210998@gmail.com>
2019-07-05 18:17:35 +0700

    Signed-off-by: minh cuong <cuongnguyen.210998@gmail.com>

--- exam5/view/layouts/header.php
+++ exam5/view/layouts/header.php
@@ -3,6 +3,7 @@
 <!-- the head section -->
 <head>
     <title>Management Customers System</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <link rel="stylesheet" type="text/css"
           href="main.css">
 </head>
