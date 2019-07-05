4e5391cd733af6a649ca6db579ea3f8753ab6cd4
minh cuong <cuongnguyen.210998@gmail.com>
2019-07-05 18:17:13 +0700

    mvc

---
+++ exam5/view/layouts/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>Management Customers System</title>
+    <link rel="stylesheet" type="text/css"
+          href="main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>Management Customers</h1></header>
